a71d30f3ffe31244aa292f52c67c00c522903e96
tassorasso <mastme193@gmail.com>
2025-12-29 17:45:40 +0100

    buvgffffffffff
    kzftkzfgt5r6jzk

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const { createFFmpeg, fetchFile } = FFmpeg;
 const c = createFFmpeg({ log: false });
 const x = async () => {
     const f = document.getElementById('f').files[0];
-    const m = document.getElementById('m').value;
     const e = document.getElementById('e').value;
     const b = document.getElementById('b');
     const s = document.getElementById('s');
@@ -15,13 +14,9 @@ const x = async () => {
     if (!c.isLoaded()) await c.load();
     s.innerText = 'converting...';
     c.FS('writeFile', 'i', await fetchFile(f));
-    if (m === 'v2a') {
-        await c.run('-i', 'i', `o.${e}`);
-    } else {
-        await c.run('-f', 'lavfi', '-i', 'color=c=black:s=1280x720:r=1', '-i', 'i', '-shortest', `o.${e}`);
-    }
+    await c.run('-i', 'i', `o.${e}`);
     const o = c.FS('readFile', `o.${e}`);
-    const u = URL.createObjectURL(new Blob([o.buffer], { type: m === 'v2a' ? `audio/${e}` : `video/${e}` }));
+    const u = URL.createObjectURL(new Blob([o.buffer], { type: `audio/${e}` }));
     d.href = u;
     d.download = `output.${e}`;
     g.style.display = 'grid';
